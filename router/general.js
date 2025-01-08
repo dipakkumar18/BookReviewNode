ee560d944b17aadcaa3f0df22ae54c4f03404f38
dipakkumar18 <modidipak0552@gmail.com>
2025-01-08 12:47:57 +0530

    final code push

--- router/general.js
+++ router/general.js
@@ -23,10 +23,59 @@ public_users.post("/register", (req, res) => {
   return res.status(200).json({ message: "Customer succesfully registered. Now ypu can login." });
 });
 
+// Get the book list available in the shop
+public_users.get('/', function (req, res) {
+  //Write your code here
+  return res.status(200).json({ "books": books });
+});
 
+// Get book details based on ISBN
+public_users.get('/isbn/:isbn', function (req, res) {
+  //Write your code here
+  const isbn = req.params.isbn;
+  const book = Object.values(books).find(b => b.isbn === isbn);
+  if (book) {
+    return res.status(200).json(book);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
 
+// Get book details based on author
+public_users.get('/author/:author', function (req, res) {
+  //Write your code here
+  const author = req.params.author.toLowerCase();
+  const result = Object.values(books).filter(book => book.author.toLowerCase() === author);
+  if (result.length > 0) {
+    return res.status(200).json({ "booksbyauthor": result });
+  } else {
+    return res.status(404).json({ message: "Books by this author not found" });
+  }
+});
 
+// Get all books based on title
+public_users.get('/title/:title', function (req, res) {
+  //Write your code here
+  const title = req.params.title.toLowerCase();
+  const result = Object.values(books).filter(book => book.title.toLowerCase() === title);
+  if (result.length > 0) {
+    return res.status(200).json({ "booksbytitle": result });
+  } else {
+    return res.status(404).json({ message: "Books with this title not found" });
+  }
+});
 
+//  Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  //Write your code here
+  const isbn = req.params.isbn;
+  const book = Object.values(books).find(b => b.isbn === isbn);
+  if (book) {
+    return res.status(200).json(book.reviews);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
 
 
 // // Function to simulate an asynchronous fetch of all books
